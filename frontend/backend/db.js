3e54878861b6dbe96b11e7fb5cb695cf1d9ac608
trupti0406 <truptivijayyadav27@gmail.com>
2023-03-05 01:27:40 +0530

    rendering data on frontend

--- frontend/backend/db.js
+++ frontend/backend/db.js
@@ -14,12 +14,17 @@ const mongoDB = async () => {
         const fetched_data = await mongoose.connection.db.collection(
           "food-items"
         );
-        fetched_data.find({}).toArray(function (err, data) {
-          if (err) console.log(err);
-          else {
-            global.food_items = data;
-            // console.log(global.food_items);
-          }
+        fetched_data.find({}).toArray(async function (err, data) {
+          const foodCategory = await mongoose.connection.db.collection(
+            "food-category"
+          );
+          foodCategory.find({}).toArray(function (err, catData) {
+            if (err) console.log(err);
+            else {
+              global.food_items = data;
+              global.foodCategory = catData;
+            }
+          });
         });
       }
     }
